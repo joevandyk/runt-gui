dddef9da49abfd559fdb44de2d0fc0f5a007a23e
Joe Van Dyk <joe@fixieconsulting.com>
2009-04-07 16:22:18 -0700

    Refactored specs some more

--- spec/models/event_occurance_spec.rb
+++ spec/models/event_occurance_spec.rb
@@ -14,7 +14,7 @@ describe "Recurring Events" do
 
   describe "weekly event starting in march without end" do
     before(:each) do
-      @event = Event.create! :start_at => MARCH_29, :repeat_weekly => true
+      @event = Event.create! :start_at => MARCH_29, :end_at => MARCH_29 + 1.hour, :repeat_weekly => true
     end
 
     it "should have four events in april" do
@@ -23,12 +23,19 @@ describe "Recurring Events" do
       end
     end
 
-    it "the created occurances should be attached to the event, and have the same times for starting" do
+    it "the created occurances should be attached to the event, and have the same times for starting and ending" do
       april_events = EventOccurance.for_month(APRIL)
       april_events[0].start_at.should == DateTime.new(2009, 4,  5, 19, 30)
+      april_events[0].end_at.  should == DateTime.new(2009, 4,  5, 20, 30)
+
       april_events[1].start_at.should == DateTime.new(2009, 4, 12, 19, 30)
+      april_events[1].end_at.  should == DateTime.new(2009, 4, 12, 20, 30)
+
       april_events[2].start_at.should == DateTime.new(2009, 4, 19, 19, 30)
+      april_events[2].end_at.  should == DateTime.new(2009, 4, 19, 20, 30)
+
       april_events[3].start_at.should == DateTime.new(2009, 4, 26, 19, 30)
+      april_events[3].end_at.  should == DateTime.new(2009, 4, 26, 20, 30)
     end
 
     it "shouldn't create new events on subsequent calls" do
@@ -54,20 +61,10 @@ describe "Recurring Events" do
   end
 
   describe "weekly event starting in march ending midway through april" do
-    before(:each) do
+    it "should have three events in april (not 4)" do
       @event = Event.create! :start_at => MARCH_29, :repeat_weekly => true, :end_at => MARCH_29 + 1.hour, :events_end_at => APRIL_19
       @april = EventOccurance.for_month(APRIL)
-    end
-
-    it "should have three events in april" do
       @april.size.should == 3
     end
-
-    it "should set the end times on the occurances" do
-      @april[0].end_at.should == DateTime.new(2009, 4,  5, 20, 30)
-      @april[1].end_at.should == DateTime.new(2009, 4, 12, 20, 30)
-      @april[2].end_at.should == DateTime.new(2009, 4, 19, 20, 30)
-    end
-
   end
 end
