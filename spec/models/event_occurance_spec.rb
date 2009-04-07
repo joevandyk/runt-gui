4a2933fbcb4ae12fe5da0b6ce3afe7e8c2680456
Joe Van Dyk <joe@fixieconsulting.com>
2009-04-07 16:11:39 -0700

    Refactored specs a bit

--- spec/models/event_occurance_spec.rb
+++ spec/models/event_occurance_spec.rb
@@ -1,58 +1,63 @@
 require 'spec/spec_helper'
 
 describe "Recurring Events" do
+
+  FEB       = DateTime.new(2009, 2)
+  MARCH     = DateTime.new(2009, 3)
+  APRIL     = DateTime.new(2009, 4)
+  MARCH_29  = DateTime.new(2009, 3, 29, 19, 30)
+  APRIL_19  = DateTime.new(2009, 4, 19)
+
   before(:each) do
     Event.destroy_all
   end
 
   describe "weekly event starting in march without end" do
     before(:each) do
-      @event = Event.create! :start_at => DateTime.new(2009, 3, 29, 19, 30), :repeat_weekly => true
+      @event = Event.create! :start_at => MARCH_29, :repeat_weekly => true
     end
 
     it "should have four events in april" do
       assert_difference "EventOccurance.count", 4 do 
-        EventOccurance.for_month(DateTime.new(2009, 4)).size.should == 4
+        EventOccurance.for_month(APRIL).size.should == 4
       end
     end
 
     it "the created occurances should be attached to the event, and have the same times for starting" do
-      EventOccurance.for_month(DateTime.new(2009, 4)).each do |e|
+      EventOccurance.for_month(APRIL).each do |e|
         e.start_at.wday.should == 0
         e.start_at.hour.should == @event.start_at.hour
-        e.start_at.min.should == @event.start_at.min
-        e.event.should == @event
+        e.start_at.min.should  == @event.start_at.min
+        e.event.should         == @event
       end
     end
 
     it "shouldn't create new events on subsequent calls" do
-      april = EventOccurance.for_month(DateTime.new(2009, 4))
+      april = EventOccurance.for_month(APRIL)
       assert_difference "EventOccurance.count", 0 do 
-        april.should == EventOccurance.for_month(DateTime.new(2009, 4))
+        april.should == EventOccurance.for_month(APRIL)
       end
     end
 
     it "should be able to find events in far future" do
       assert_difference "EventOccurance.count", 4 do 
-        EventOccurance.for_month(DateTime.new(2010, 4)).size.should == 4
+        EventOccurance.for_month(APRIL + 1.year).size.should == 4
       end
     end
 
     it "should be one event in march" do
-      march = EventOccurance.for_month(DateTime.new(2009, 3))
-      march.size.should == 1
+      EventOccurance.for_month(MARCH).size.should == 1
     end
 
     it "should be no events in feb" do
-      feb = EventOccurance.for_month(DateTime.new(2009, 2))
-      feb.size.should == 0
+      EventOccurance.for_month(FEB).should be_blank
     end
   end
 
   describe "weekly event starting in march ending midway through april" do
     before(:each) do
-      @event = Event.create! :start_at => DateTime.new(2009, 3, 29, 19, 30), :repeat_weekly => true, :end_at => DateTime.new(2009, 4, 19)
-      @april = EventOccurance.for_month(DateTime.new(2009, 4))
+      @event = Event.create! :start_at => MARCH_29, :repeat_weekly => true, :end_at => APRIL_19
+      @april = EventOccurance.for_month(APRIL)
     end
 
     it "should have three events in april" do
@@ -63,7 +68,7 @@ describe "Recurring Events" do
       @april.each do |e|
         e.end_at.wday.should == 0
         e.end_at.hour.should == @event.end_at.hour
-        e.end_at.min.should == @event.end_at.min
+        e.end_at.min.should  == @event.end_at.min
       end
     end
 
